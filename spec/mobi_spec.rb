129898eb3416a2c687e8a7031d2e811357a73820
Marat Radchenko <marat@slonopotamus.org>
2020-09-06 21:04:14 +0300

    Split PalmDB/Mobi into separate classes (#6)

--- spec/mobi_spec.rb
+++ spec/mobi_spec.rb
@@ -3,75 +3,17 @@
 require 'spec_helper'
 require 'time'
 
-RSpec.shared_examples 'sample book' do # rubocop:disable Metrics/BlockLength
+RSpec.shared_examples 'sample Mobi' do
   it 'is not nil' do
     expect(subject).not_to be_nil
   end
 
-  it 'has name' do
-    expect(subject.name).to eq('Asciidoctor_-Sample_Content')
-  end
-
-  it 'has attributes' do
-    expect(subject.attributes).to eq(0)
-  end
-
-  it 'has version' do
-    expect(subject.version).to eq(0)
-  end
-
-  it 'has ctime' do
-    expect(subject.ctime).to eq(Time.parse('2020-09-01 18:52:26 UTC'))
-  end
-
-  it 'has mtime' do
-    expect(subject.mtime).to eq(Time.parse('2020-09-01 18:52:27 UTC'))
-  end
-
-  it 'has btime' do
-    expect(subject.btime).to eq(Time.at(0))
-  end
-
-  it 'has mod_num' do
-    expect(subject.mod_num).to eq(0)
-  end
-
-  it 'has appinfo_offset' do
-    expect(subject.appinfo_offset).to eq(0)
-  end
-
-  it 'has sortinfo_offset' do
-    expect(subject.appinfo_offset).to eq(0)
-  end
-
-  it 'has type' do
-    expect(subject.type).to eq(Dyck::Mobi::BOOK_MAGIC)
-  end
-
-  it 'has creator' do
-    expect(subject.creator).to eq(Dyck::Mobi::MOBI_MAGIC)
-  end
-
-  it 'has uid' do
-    expect(subject.uid).to eq(123)
-  end
-
-  it 'has next_rec' do
-    expect(subject.next_rec).to eq(0)
-  end
-
-  it 'has records' do
-    expect(subject.records.size).to eq(61)
-    expect(subject.records[42].uid).to eq(86)
-    expect(subject.records[42].body).to start_with('w:0002?mime=text/css);/* @page is for EPUB2 only */')
-  end
-
   it 'has KF7 header' do
     expect(subject.kf7).not_to be_nil
-    expect(subject.kf7.compression).to eq(Dyck::Mobi::NO_COMPRESSION)
-    expect(subject.kf7.encryption).to eq(Dyck::Mobi::NO_ENCRYPTION)
+    expect(subject.kf7.compression).to eq(Dyck::MobiData::NO_COMPRESSION)
+    expect(subject.kf7.encryption).to eq(Dyck::MobiData::NO_ENCRYPTION)
     expect(subject.kf7.mobi_type).to eq(2)
-    expect(subject.kf7.text_encoding).to eq(Dyck::Mobi::TEXT_ENCODING_UTF8)
+    expect(subject.kf7.text_encoding).to eq(Dyck::MobiData::TEXT_ENCODING_UTF8)
     expect(subject.kf7.version).to eq(6)
     expect(subject.kf7.exth_records.size).to eq(28)
     expect(subject.kf7.exth_records[2].tag).to eq(100)
@@ -80,10 +22,10 @@ RSpec.shared_examples 'sample book' do # rubocop:disable Metrics/BlockLength
 
   it 'has KF8 header' do
     expect(subject.kf8).not_to be_nil
-    expect(subject.kf8.compression).to eq(Dyck::Mobi::NO_COMPRESSION)
-    expect(subject.kf8.encryption).to eq(Dyck::Mobi::NO_ENCRYPTION)
+    expect(subject.kf8.compression).to eq(Dyck::MobiData::NO_COMPRESSION)
+    expect(subject.kf8.encryption).to eq(Dyck::MobiData::NO_ENCRYPTION)
     expect(subject.kf8.mobi_type).to eq(2)
-    expect(subject.kf8.text_encoding).to eq(Dyck::Mobi::TEXT_ENCODING_UTF8)
+    expect(subject.kf8.text_encoding).to eq(Dyck::MobiData::TEXT_ENCODING_UTF8)
     expect(subject.kf8.version).to eq(8)
     expect(subject.kf8.exth_records.size).to eq(27)
     expect(subject.kf8.exth_records[2].tag).to eq(100)
@@ -96,7 +38,7 @@ describe 'existing file' do
     Dyck::Mobi.read(fixture_file('sample-book.mobi'))
   end
 
-  it_behaves_like 'sample book'
+  it_behaves_like 'sample Mobi'
 end
 
 describe 'copy created by Dyck' do
@@ -107,7 +49,7 @@ describe 'copy created by Dyck' do
     Dyck::Mobi.read(io)
   end
 
-  it_behaves_like 'sample book'
+  it_behaves_like 'sample Mobi'
 end
 
 describe 'empty Mobi' do
@@ -137,7 +79,7 @@ describe 'empty KF8 Mobi' do
 
   it 'has KF7 header with KF8 boundary' do
     expect(subject.kf7).not_to be_nil
-    expect(subject.kf7.find_exth(Dyck::ExthRecord::EXTH_KF8BOUNDARY)).not_to be_nil
+    expect(subject.kf7.find_exth(Dyck::ExthRecord::KF8_BOUNDARY)).not_to be_nil
   end
 
   it 'has KF8 header' do
---
+++ spec/palmdb_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'time'
+
+RSpec.shared_examples 'sample PalmDB' do # rubocop:disable Metrics/BlockLength
+  it 'is not nil' do
+    expect(subject).not_to be_nil
+  end
+
+  it 'has name' do
+    expect(subject.name).to eq('Asciidoctor_-Sample_Content')
+  end
+
+  it 'has attributes' do
+    expect(subject.attributes).to eq(0)
+  end
+
+  it 'has version' do
+    expect(subject.version).to eq(0)
+  end
+
+  it 'has ctime' do
+    expect(subject.ctime).to eq(Time.parse('2020-09-01 18:52:26 UTC'))
+  end
+
+  it 'has mtime' do
+    expect(subject.mtime).to eq(Time.parse('2020-09-01 18:52:27 UTC'))
+  end
+
+  it 'has btime' do
+    expect(subject.btime).to eq(Time.at(0))
+  end
+
+  it 'has mod_num' do
+    expect(subject.mod_num).to eq(0)
+  end
+
+  it 'has appinfo_offset' do
+    expect(subject.appinfo_offset).to eq(0)
+  end
+
+  it 'has sortinfo_offset' do
+    expect(subject.appinfo_offset).to eq(0)
+  end
+
+  it 'has type' do
+    expect(subject.type).to eq(Dyck::Mobi::TYPE_MAGIC)
+  end
+
+  it 'has creator' do
+    expect(subject.creator).to eq(Dyck::Mobi::CREATOR_MAGIC)
+  end
+
+  it 'has uid' do
+    expect(subject.uid).to eq(123)
+  end
+
+  it 'has next_rec' do
+    expect(subject.next_rec).to eq(0)
+  end
+
+  it 'has records' do
+    expect(subject.records.size).to eq(61)
+    expect(subject.records[42].uid).to eq(86)
+    expect(subject.records[42].body).to start_with('w:0002?mime=text/css);/* @page is for EPUB2 only */')
+  end
+end
+
+describe 'existing file' do
+  subject do
+    Dyck::PalmDB.read(fixture_file('sample-book.mobi'))
+  end
+
+  it_behaves_like 'sample PalmDB'
+end
+
+describe 'copy created by Dyck' do
+  subject do
+    original = Dyck::PalmDB.read(fixture_file('sample-book.mobi'))
+    io = original.write(StringIO.new)
+    io.seek(0)
+    Dyck::PalmDB.read(io)
+  end
+
+  it_behaves_like 'sample PalmDB'
+end
+
+describe 'empty PalmDB' do
+  subject { Dyck::PalmDB.new }
+
+  it 'does not change after save/load' do
+    io = subject.write(StringIO.new)
+    io.seek(0)
+    palmdb = Dyck::PalmDB.read(io)
+
+    expect(Marshal.dump(palmdb)).to eq(Marshal.dump(subject))
+  end
+end
