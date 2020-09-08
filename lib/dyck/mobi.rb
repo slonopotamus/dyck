4659a1ca53fac3cb2c0a8628e4ad64161b23aece
Marat Radchenko <marat@slonopotamus.org>
2020-09-08 10:15:06 +0300

    Implement FDST read/write

--- lib/dyck/mobi.rb
+++ lib/dyck/mobi.rb
@@ -40,6 +40,9 @@ module Dyck
     MOBI_MAGIC = 'MOBI'.b
     EXTH_MAGIC = 'EXTH'.b
 
+    FDST_MAGIC = 'FDST'.b
+    FDST_HEADER = %(A#{FDST_MAGIC.bytesize}NN)
+
     MAX_RECORD_SIZE = 4096
 
     attr_accessor(:compression)
@@ -49,8 +52,8 @@ module Dyck
     attr_accessor(:version)
     # @return [Array<Dyck::ExthRecord>]
     attr_reader(:exth_records)
-    # @return [String]
-    attr_accessor(:content)
+    # @return [Array<String>]
+    attr_accessor(:flow)
 
     def initialize( # rubocop:disable Metrics/ParameterLists
       compression: NO_COMPRESSION,
@@ -59,7 +62,7 @@ module Dyck
       text_encoding: TEXT_ENCODING_UTF8,
       version: 6,
       exth_records: [],
-      content: ''.b
+      flow: []
     )
       @compression = compression
       @encryption = encryption
@@ -67,13 +70,7 @@ module Dyck
       @text_encoding = text_encoding
       @version = version
       @exth_records = exth_records
-      @content = content
-    end
-
-    def content_chunks
-      content.bytes.each_slice(MobiData::MAX_RECORD_SIZE).map do |chunk|
-        PalmDBRecord.new(content: chunk.pack('c*'))
-      end
+      @flow = flow
     end
 
     class << self
@@ -93,24 +90,59 @@ module Dyck
           raise ArgumentError, %(Unsupported encryption: #{result.encryption})
         end
 
-        extra_flags = read_mobi_header(result, io)
-        read_exth_header(result, io)
+        extra_flags, fdst_index, fdst_section_count = read_mobi_header(result, io)
+        result.exth_records.concat(read_exth_header(io))
 
-        (1..text_record_count).each do |idx|
-          content = records[index + idx].content
-          extra_size = get_record_extra_size(content, extra_flags)
-          bytesize = content.bytesize
-          len = bytesize - extra_size
-          result.content += content[0..len - 1]
-        end
+        content = read_content(index + 1, text_record_count, records, extra_flags)
+        fdst = read_fdst(fdst_index, index, records, fdst_section_count)
+        result.flow = if fdst.nil?
+                        content.empty? ? [] : [content]
+                      else
+                        fdst.map do |range|
+                          content[range.begin..range.end - 1]
+                        end
+                      end
 
         result
       end
 
       private
 
+      # @param offset [Fixnum]
+      # @param count [Fixnum]
+      # @param records [Array<PalmDBRecord>]
+      # @return [String]
+      def read_content(offset, count, records, extra_flags)
+        content = ''.b
+        (0..count - 1).each do |idx|
+          record_content = records[offset + idx].content
+          extra_size = get_record_extra_size(record_content, extra_flags)
+          content += record_content[0..record_content.bytesize - extra_size - 1]
+        end
+        content
+      end
+
+      # @param fdst_index [Fixnum, nil]
+      # @param offset [Fixnum]
+      # @param records [Array<Dyck::PalmDBRecord>]
+      # @return [Array<Range>]
+      def read_fdst(fdst_index, offset, records, fdst_section_count)
+        return nil if fdst_index.nil? || fdst_section_count.nil? || fdst_section_count <= 1
+
+        fdst_record = records[offset + fdst_index]
+        io = StringIO.new(fdst_record.content)
+        magic, _data_offset, _section_count = io.read(12).unpack(FDST_HEADER)
+        raise ArgumentError, %(Unsupported FDST magic: #{magic}) if magic != FDST_MAGIC
+
+        (0..fdst_section_count - 1).map do |_|
+          start, end_ = io.read(8).unpack('NN')
+          (start..end_)
+        end
+      end
+
       # @param data [String]
       # @param offset [Fixnum]
+      # @return [Fixnum]
       def get_varlen_dec(data, offset)
         bitpos = 0
         result = 0
@@ -119,8 +151,9 @@ module Dyck
           result |= (v & 0x7F) << bitpos
           bitpos += 7
           offset -= 1
-          return result if (v & 0x80) != 0 || offset.zero?
+          break if (v & 0x80) != 0 || offset.zero?
         end
+        result
       end
 
       # @param data [String]
@@ -139,55 +172,67 @@ module Dyck
 
       # @param mobi_data [Dyck::MobiData]
       # @param io [IO, StringIO]
-      def read_mobi_header(mobi_data, io) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      def read_mobi_header(mobi_data, io) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
         magic = io.read(4)
         raise ArgumentError, %(Unsupported magic: #{magic}) if magic != MOBI_MAGIC
 
         mobi_header_length = io.read(4).unpack1('N') - 8
         mobi_header = StringIO.new(io.read(mobi_header_length))
-        mobi_data.mobi_type, mobi_data.text_encoding, _uid, mobi_data.version = mobi_header.read(16).unpack('N*')
-
-        # ignore all the rest Mobi header fields for now
-        mobi_header.seek(202, IO::SEEK_CUR)
-        extra_flags = mobi_header.read(2)&.unpack1('n') || 0
-        # there are more fields *after* extra_flags
+        mobi_data.mobi_type, mobi_data.text_encoding, _uid, mobi_data.version, = mobi_header.read(42 * 4).unpack('N*')
 
         unless SUPPORTED_TEXT_ENCODINGS.include?(mobi_data.text_encoding)
           raise ArgumentError, %(Unsupported text encoding: #{mobi_data.text_encoding})
         end
 
-        extra_flags
+        # there are more fields here
+        if mobi_data.version >= 8
+          fdst_index = mobi_header.read(4)&.unpack1('N')
+        else
+          # Assume that last_text_index is fdst_index for KF7
+          _, fdst_index = mobi_header.read(4)&.unpack('nn')
+        end
+        fdst_section_count = mobi_header.read(4)&.unpack1('N')
+        # there are more fields here
+        mobi_header.seek(42, IO::SEEK_CUR)
+        extra_flags = mobi_header.read(2)&.unpack1('n') || 0
+        # there are more fields here
+
+        [extra_flags, fdst_index, fdst_section_count]
       end
 
-      # @param mobi_data [Dyck::MobiData]
       # @param io [IO, StringIO]
-      def read_exth_header(mobi_data, io) # rubocop:disable Metrics/AbcSize
+      # @return [Array<Dyck::ExthRecord]
+      def read_exth_header(io)
         exth = io.read(4)
         raise ArgumentError, %(Unsupported EXTH: #{exth}) if exth != EXTH_MAGIC
 
         exth_header_length, exth_record_count = io.read(8).unpack('N*')
         exth_header = StringIO.new(io.read(exth_header_length - 12))
-        (0..exth_record_count - 1).each do |_|
+        (0..exth_record_count - 1).map do |_|
           tag, data_len = exth_header.read(8).unpack('N*')
           data = exth_header.read(data_len - 8)
-          mobi_data.exth_records << ExthRecord.new(tag: tag, data: data)
+          ExthRecord.new(tag: tag, data: data)
         end
       end
     end
 
-    # @param record [Dyck::PalmDBRecord]
-    def write(record, text_record_count)
+    # @param header_record [Dyck::PalmDBRecord]
+    # @param text_record_count [Fixnum]
+    # @param fdst_index [Fixnum]
+    def write(header_record, text_record_count, fdst_index)
       io = StringIO.new
       io.binmode
       text_length = 0 # TODO
       io.write([compression, 0, text_length, text_record_count, MAX_RECORD_SIZE, encryption, 0].pack('nnNn*'))
 
-      write_mobi_header(io)
+      write_mobi_header(io, fdst_index, @flow.size)
       write_exth_header(io)
 
-      record.content = io.string
+      header_record.content = io.string
     end
 
+    # @param tag [String]
+    # @param data [String]
     def set_exth(tag, data)
       record = find_exth(tag)
       if record.nil?
@@ -197,21 +242,49 @@ module Dyck
       end
     end
 
+    # @param tag [String]
     def remove_exth(tag)
       @exth_records.reject! { |record| record.tag == tag }
     end
 
+    # @return [Dyck::ExthRecord, nil]
     def find_exth(tag)
       @exth_records.detect { |record| record.tag == tag }
+    end
+
+    # @return [Array<Dyck::PalmDBRecord]
+    def content_chunks
+      @flow.join.bytes.each_slice(MobiData::MAX_RECORD_SIZE).map do |chunk|
+        PalmDBRecord.new(content: chunk.pack('c*'))
+      end
+    end
+
+    # @param fdst_record [Dyck::PalmDBRecord]
+    def write_fdst(fdst_record)
+      io = StringIO.new
+      io.binmode
+      io.write([FDST_MAGIC, 12, @flow.size].pack(%(A#{FDST_MAGIC.bytesize}NN)))
+      start = 0
+      @flow.each do |f|
+        end_ = start + f.bytesize
+        io.write([start, end_].pack('NN'))
+        start = end_
+      end
+      fdst_record.content = io.string
     end
 
     private
 
     # @param io [IO, StringIO]
-    def write_mobi_header(io)
-      header_length = 24
+    # @param fdst_index [Fixnum]
+    # @param fdst_section_count [Fixnum]
+    def write_mobi_header(io, fdst_index, fdst_section_count)
+      header_length = 184
       uid = 0
-      io.write([MOBI_MAGIC, header_length, @mobi_type, @text_encoding, uid, @version].pack('a*N*'))
+      io.write([MOBI_MAGIC, header_length, @mobi_type, @text_encoding, uid, @version]
+                   .concat([0xffffffff] * 38)
+                   .concat([fdst_index || 0, fdst_section_count])
+                   .pack(%(A#{MOBI_MAGIC.bytesize}N*)))
     end
 
     # @param io [IO, StringIO]
@@ -280,12 +353,16 @@ module Dyck
 
       @kf7.remove_exth(ExthRecord::KF8_BOUNDARY)
       if @kf8
-        @kf7.set_exth(ExthRecord::KF8_BOUNDARY, [palmdb.records.size].pack('N'))
+        kf8_boundary = palmdb.records.size
+        @kf7.set_exth(ExthRecord::KF8_BOUNDARY, [kf8_boundary].pack('N'))
         palmdb.records << (kf8_header = PalmDBRecord.new)
         palmdb.records.concat(kf8_content_chunks = @kf8.content_chunks)
-        @kf8.write(kf8_header, kf8_content_chunks.size)
+        fdst_index = palmdb.records.size - kf8_boundary
+        palmdb.records << (fdst_record = PalmDBRecord.new)
+        @kf8.write_fdst(fdst_record)
+        @kf8.write(kf8_header, kf8_content_chunks.size, fdst_index)
       end
-      @kf7.write(kf7_header, kf7_content_chunks.size)
+      @kf7.write(kf7_header, kf7_content_chunks.size, 0)
       palmdb
     end
   end
