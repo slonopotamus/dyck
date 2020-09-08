acdfd62be2adbff117550901d9d733a7a79a6440
Marat Radchenko <marat@slonopotamus.org>
2020-09-08 21:00:57 +0300

    Store EXTH metadata as Mobi object attributes (#10)

--- spec/mobi_spec.rb
+++ spec/mobi_spec.rb
@@ -8,6 +8,18 @@ RSpec.shared_examples 'sample Mobi' do # rubocop:disable Metrics/BlockLength
     expect(subject).not_to be_nil
   end
 
+  it 'has title' do
+    expect(subject.title).to eq('')
+  end
+
+  it 'has author' do
+    expect(subject.author).to eq('Sarah White')
+  end
+
+  it 'has publisher' do
+    expect(subject.publisher).to eq('Asciidoctor')
+  end
+
   it 'has KF7 header' do
     expect(subject.kf7).not_to be_nil
     expect(subject.kf7.compression).to eq(Dyck::MobiData::NO_COMPRESSION)
@@ -15,9 +27,6 @@ RSpec.shared_examples 'sample Mobi' do # rubocop:disable Metrics/BlockLength
     expect(subject.kf7.mobi_type).to eq(2)
     expect(subject.kf7.text_encoding).to eq(Dyck::MobiData::TEXT_ENCODING_UTF8)
     expect(subject.kf7.version).to eq(6)
-    expect(subject.kf7.exth_records.size).to eq(28)
-    expect(subject.kf7.exth_records[2].tag).to eq(100)
-    expect(subject.kf7.exth_records[2].data).to eq('Sarah White')
   end
 
   it 'has KF7 flow' do
@@ -31,9 +40,6 @@ RSpec.shared_examples 'sample Mobi' do # rubocop:disable Metrics/BlockLength
     expect(subject.kf8.mobi_type).to eq(2)
     expect(subject.kf8.text_encoding).to eq(Dyck::MobiData::TEXT_ENCODING_UTF8)
     expect(subject.kf8.version).to eq(8)
-    expect(subject.kf8.exth_records.size).to eq(27)
-    expect(subject.kf8.exth_records[2].tag).to eq(100)
-    expect(subject.kf8.exth_records[2].data).to eq('Sarah White')
   end
 
   it 'has KF8 flow' do
@@ -93,7 +99,6 @@ describe 'empty KF8 Mobi' do
 
   it 'has KF7 header with KF8 boundary' do
     expect(subject.kf7).not_to be_nil
-    expect(subject.kf7.find_exth(Dyck::ExthRecord::KF8_BOUNDARY)).not_to be_nil
   end
 
   it 'has KF8 header' do
